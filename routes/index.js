ab7b071abb4e1c1977502e771867adddaebc89a0
Youli Pan <yopan@ucsd.edu>
2018-02-21 22:40:06 -0800

    alt

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ var projects = require('../projects.json');
 
 exports.view = function(request, response){
   	projects["viewAlt"]=false;
-  	res.render('index', projects);
+  	response.render('index', projects);
   	
 };
 
 exports.viewAlt = function(request,response){
 	project["viewAlt"]=true;
-	res.render('index', projects);
+	response.render('index', projects);
 
 };
 
